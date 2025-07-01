a2198a399eddad1a306082351561f49b995dc5d8
ssdko2004 <ssdko2004@gmail.com>
2025-07-01 13:51:53 -0500

    Added Player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pass_count, fail_count = pygame.init()
@@ -7,12 +8,23 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    starting_x = SCREEN_WIDTH / 2
+    starting_y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(starting_x, starting_y)
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
 
         screen.fill("black")
+
+        for item in drawable:
+            item.draw(screen)
 
 
         pygame.display.flip()
