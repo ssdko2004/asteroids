33cf1d34ee225b465f5540e7ef79cd068c4f218f
ssdko2004 <ssdko2004@gmail.com>
2025-07-01 09:26:19 -0500

    Limit to 60 FPS

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def main():
     pass_count, fail_count = pygame.init()
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -15,6 +16,7 @@ def main():
 
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
